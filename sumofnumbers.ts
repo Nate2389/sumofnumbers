c3a3e059f212f3058e961a638ac34c477142c27c
Nathan Chee <nkc8923@hawaii.edu>
2024-09-21 13:09:52 -1000

    Done at 21:07

---
+++ sumofnumbers.ts
@@ -0,0 +1,44 @@
+const numList = [1, 2, 3, 4];
+
+function sumFor(list: number[]): number {
+  let total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+  return total;
+}
+
+console.log(sumFor(numList));
+
+function sumWhile(list: number[]): number {
+  let total = 0;
+  let i = list.length - 1;
+  while (i >= 0) {
+    total += list[i];
+    i--;
+  }
+  return total;
+}
+
+console.log(sumWhile(numList));
+
+function sumRecursion(list: number[]): number {
+  let total = 0;
+  if (list.length === 0) {
+    return 0;
+  }
+  total += list[0] + sumRecursion(list.slice(1));
+  return total;
+}
+
+console.log(sumRecursion(numList));
+
+function sumTheFunctionalWay(list: number[]): number {
+  let total = 0;
+  list.forEach((num) => {
+    total += num;
+  });
+  return total;
+}
+
+console.log(sumTheFunctionalWay(numList));
